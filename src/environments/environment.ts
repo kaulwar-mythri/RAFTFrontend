cf0cfbd3f9d6f9153f2dce7d280e69a32597587c
Kaulwar Mythri <kaulwar.mythri@accolitedigital.com>
2024-02-02 14:26:57 +0530

    host website on firebase

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
     firebase: {
         apiKey: "AIzaSyATGpbm68f-R3ie7NdEfdHEGzqYEJwlNFA",
-        authDomain: "fir-3923e.firebaseapp.com",
-        projectId: "fir-3923e",
-        storageBucket: "fir-3923e.appspot.com",
-        messagingSenderId: "878025415869",
-        appId: "1:878025415869:web:5838ef488581eaae6b2bd4"
+  authDomain: "fir-3923e.firebaseapp.com",
+  projectId: "fir-3923e",
+  storageBucket: "fir-3923e.appspot.com",
+  messagingSenderId: "878025415869",
+  appId: "1:878025415869:web:4ec88c9177e7cb546b2bd4"
     },
     production: false
 }
